c2c19e7d70be0d98a4aa0f3a1f81596cdabb9797
bhagyesh2001 <146858005+bhagyesh2001@users.noreply.github.com>
2024-04-01 13:31:07 +0530

    Add files via upload

---
+++ StringManipulation.java
@@ -0,0 +1,7 @@
+import java.util.Scanner
+public clas StringManipulation{
+public static void main(String[]args){
+Scanner scanner=new Scanner(System.in);
+System.out.println("enter a string for manipulation");
+String inputString=scanner.nextLine();
+
---
+++ sort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.scanner;
+class sort
+{
+public static void main(String args[])
+{
+int n;
+string temp;
+Scanner s=new Scanner(System.in);
+System.out.println("enter no. of names:");
+n=s.nextInt();
+String names[]=new String[n];
+Scanner s1=new Scanner
+}
+} 
